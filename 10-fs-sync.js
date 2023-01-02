123df54826a3df99649101c21a2c6779ae00d129
İHLASMERT <ihlas1mert@gmail.com>
2023-01-02 15:43:43 +0300

    added sync

---
+++ 10-fs-sync.js
@@ -0,0 +1,8 @@
+const first = readFileSync("./content/first.txt", "utf8");
+const second = readFileSync("./content/second.txt", "utf8");
+
+console.log(first, second);
+
+writeFileSync(
+  "./content/result-sync.text",
+  `Here is the result: ${first}, ${second}`, {flag: "a"})
--- app.js
+++ app.js
@@ -1,6 +1,2 @@
-const { readFileSync, writeFileSync } = require("fs");
+const { readFile, writeFile } = require("fs");
 
-const first = readFileSync("./content/first.txt", "utf8");
-const second = readFileSync("./content/second.txt", "utf8");
-
-console.log(first, second);
