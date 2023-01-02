07057578983580d5e75028e8dae40fd28bbebe34
İHLASMERT <ihlas1mert@gmail.com>
2023-01-02 15:01:50 +0300

    added alternative flavor

---
+++ 4-names.js
@@ -0,0 +1,6 @@
+const jahan = "jahan"
+const pena = "pena"
+const secret = "SUPER SECRET"
+
+
+module.exports = {jahan, pena}
---
+++ app.js
@@ -0,0 +1,9 @@
+//Modules - Encapsulated code(only share minimum)
+// CommonJSi every file is module (by default)
+
+
+
+
+sayHi("susan")
+sayHi(jahan)
+sayHi(pena)
